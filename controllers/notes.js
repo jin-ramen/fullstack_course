8dec80d3a5cad0c1978dcb5e382c16080698d996
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2025-05-09 11:50:41 +0300

    Add error handling is userId doesn't exist

--- controllers/notes.js
+++ controllers/notes.js
@@ -22,6 +22,10 @@ notesRouter.post('/', async (request, response) => {
 
   const user = await User.findById(body.userId)
 
+  if (!user) {
+    return response.status(400).json({ error: 'UserId missing or not valid' })
+  }
+
   const note = new Note({
     content: body.content,
     important: body.important || false,
