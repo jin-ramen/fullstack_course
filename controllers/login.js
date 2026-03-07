70b3e6eada4a39c6e4707f15e32adcf5c7505018
Jin Hong Pang <jinhong36@icloud.com>
2026-03-07 19:19:44 +1100

    feat: add user authentication and user management features
    - Implemented login functionality with JWT and bcrypt for password hashing.
    - Created user registration endpoint to add new users with unique usernames.
    - Added user model with username, name, and passwordHash fields.
    - Updated middleware to handle duplicate username errors.
    - Removed outdated average and reverse tests, and added new tests for user and note APIs.
    - Enhanced logger to suppress logs during testing.
    - Updated package.json to include new dependencies: bcrypt, jsonwebtoken, and supertest.

---
+++ controllers/login.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const loginRouter = require('express').Router()
+const User = require('../models/user')
+
+loginRouter.post('/', async (request, response) => {
+  const { username, password } = request.body
+  // search for the user in the db
+  const user = await User.findOne({ username })
+  // check password
+  const passwordCorrect = user === null
+    ? false
+    : await bcrypt.compare(password, user.passwordHash)
+
+  // if user DNE or incorrect password
+  if (!(user && passwordCorrect)) {
+    return response.status(401).json({
+      error: 'invalid username or password'
+    })
+  }
+
+  // token contain username and user id in digitally signed form
+  const userForToken = {
+    username: user.username,
+    id: user._id,
+  }
+  const token = jwt.sign(userForToken, process.env.SECRET)
+
+  // success request response
+  response
+    .status(200)
+    .send({ token, username: user.username, name: user.name })
+})
+
+module.exports = loginRouter
